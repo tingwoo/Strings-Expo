e82859f70a15d877d9918bd0a863f75a1ac121f0
tingwu <tingwu92220205@gmail.com>
2024-02-13 16:28:22 +0800

    performance improvement, simple interaction

--- sketch.js
+++ sketch.js
@@ -27,10 +27,18 @@ let totalLines = 5000;
 
 let fr = 0;
 
-function getColorFromPixel(img, x, y) {
-  let w = img.width;
+let showFrameRate = true;
+
+let draggingHandle = false;
+let handleAngle = -Math.PI / 4;
+let handleAngleDragging = 0;
+
+let timer;
+
+function getColorFromPixel(x, y) {
+  let w = video.width;
   let i = (y*w+x)*4;
-  return [img.pixels[i], img.pixels[i+1], img.pixels[i+2], img.pixels[i+3]];
+  return [video.pixels[i], video.pixels[i+1], video.pixels[i+2], video.pixels[i+3]];
 }
 
 function filt(bri) {
@@ -45,7 +53,7 @@ function filt(bri) {
 }
 
 function getVector(first, second) {
-  var slope = (first[1] - second[1]) / (first[0] - second[0]);
+  let slope = (first[1] - second[1]) / (first[0] - second[0]);
   if(Math.abs(slope) <= 1) {
     return (first[0] - second[0] <= 0) ? [1, slope] : [-1, slope];
   } else {
@@ -72,24 +80,15 @@ function pixelToBlockDiscrete(coord) {
 }
 
 function getLineBlocks(first, second){
-  if(first == second) return [];
-  if(first > second) return getLineBlocks(second, first);
+  if(first === second) return [];
+  if(first > second) return blocksInEachLine[second][first - second - 1];
   return blocksInEachLine[first][second - first - 1];
 }
 
 function getVideoSample(x, y) {
-  let c = getColorFromPixel(video, (horizontalRes-x-gap)*videoPixelPerBlock, y*videoPixelPerBlock);
+  let c = getColorFromPixel((horizontalRes-x-gap)*videoPixelPerBlock, y*videoPixelPerBlock);
   return filt(brightness(c) * 2.55);
 }
-
-// function getVideoSample(x, y) {
-//   let x2 = horizontalRes-x-gap;
-//   let c0 = getColorFromPixel(video, x2*videoPixelPerBlock,   y*videoPixelPerBlock);
-//   let c1 = getColorFromPixel(video, x2*videoPixelPerBlock+1, y*videoPixelPerBlock);
-//   let c2 = getColorFromPixel(video, x2*videoPixelPerBlock,   y*videoPixelPerBlock+1);
-//   let c3 = getColorFromPixel(video, x2*videoPixelPerBlock+1, y*videoPixelPerBlock+1);
-//   return filt((brightness(c0) + brightness(c1) + brightness(c2) + brightness(c3)) * 2.55 / 4);
-// }
 
 function diffFormula(canvas, truth) {
   // if(canvas > truth) {
@@ -97,6 +96,36 @@ function diffFormula(canvas, truth) {
   // }
   // return truth - canvas;
   return Math.abs(truth - canvas);
+}
+
+// https://www.desmos.com/calculator/bycvo6oueu
+function handleCurveSub(x) {
+  return x / (3.5 * x + 1);
+}
+
+function handleCurve(x) {
+  if (x < -Math.PI / 4) {
+    return -handleCurveSub(-x-Math.PI/4) - Math.PI/4;
+  } else if (x < Math.PI / 4) {
+    return x;
+  } else {
+    return handleCurveSub(x-Math.PI/4) + Math.PI/4;
+  }
+}
+
+function resetTimer() {
+  timer = millis();
+  console.log("reset");
+}
+
+function timerCkpt() {
+  console.log(millis() - timer);
+  timer = millis();
+}
+
+function handleMap(angle, a, b, elastic) {
+  if(elastic) { return map(angle, -Math.PI / 4, Math.PI / 4, a, b); }
+  return constrain(map(angle, -Math.PI / 4, Math.PI / 4, a, b), a, b);
 }
 
 function setup() {
@@ -150,8 +179,6 @@ function setup() {
       currentCanvas[i].push(0);
     }
   }
-  // console.log(currentCanvas.length);
-  // console.log(currentCanvas[res-1].length);
 
   // initialize lengthOfEachLine
   for(let i = 0; i < numberOfPins; i++){
@@ -162,12 +189,22 @@ function setup() {
 }
 
 function draw() {
+  // resetTimer();
+  // timerCkpt();
   background(20); 
-  video.loadPixels();
+
+  if(frameCount % 5 === 0) { 
+    video.loadPixels(); // takes a lot of time
+  }
+
+  frameRate(60);
+  // timerCkpt();
+
+  let actualAngle = handleCurve(handleAngle + handleAngleDragging);
 
   translate(width/2-radius*scaleVar, height/2-radius*scaleVar);
   scale(scaleVar);
-  translate(-radius*distanceBetweenCenters/2, 0);
+  translate(handleMap(actualAngle, 0, -radius * distanceBetweenCenters / 2, true), 0);
 
   videoPixelPerBlock = Math.floor(video.height / res);
   horizontalRes = Math.floor(res * video.width / video.height);
@@ -193,18 +230,22 @@ function draw() {
     }
   }
 
+  // timerCkpt();
+
+  // if not able to optimize, change to 100
   for(let k = 0; k < 50; k++) {
-    // find best line
-    var bestScore = -100000000;
-    var bestLine = [];
-    var bestValuePerBlock;
+    // find the best line
+    let bestScore = -100000000;
+    let bestLine = [];
+    let bestValuePerBlock;
     for(let i1 = currentPin + 15; i1 <= currentPin + numberOfPins - 15; i1 += (Math.floor(Math.random() * 4) + 1)) {
       
       let i = i1 % numberOfPins;
 
-      if(currentLines.length > 0 && i == currentLines[currentLines.length - 1][0]) continue;
+      if(currentLines.length > 0 && i === currentLines[currentLines.length - 1][0]) continue;
 
       let blocks = getLineBlocks(currentPin, i);
+      // let blocks = currentPin < i ? blocksInEachLine[currentPin][i - currentPin - 1] : blocksInEachLine[i][currentPin - i - 1];
       let lineTotalValue = coef * res * lengthOfEachLine[Math.abs(currentPin - i)];
       let valuePerBlock = lineTotalValue / blocks.length;
       let score = 0;
@@ -232,13 +273,15 @@ function draw() {
       });
       currentPin = bestLine[1]; 
       totalLines += 1;
-      totalLines = min(5000, totalLines);
+      totalLines = Math.min(5000, totalLines);
     } else {
       totalLines -= 50;
-      totalLines = max(0, totalLines);
+      totalLines = Math.max(0, totalLines);
       break;
     }
   }
+
+  // timerCkpt();
 
   // remove first lines in queue
   if(currentLines.length > totalLines) {
@@ -255,6 +298,8 @@ function draw() {
     });
   }
 
+  // timerCkpt();
+
   // draw all lines in the queue
   stroke(255, 15);
   strokeWeight(0.8);
@@ -270,16 +315,34 @@ function draw() {
   }
 
   // show frame rate
-  if(frameCount % 30 == 0) {
-    fr = frameRate();
+  if(showFrameRate) {
+    if(frameCount % 30 === 0) {
+      fr = frameRate();
+    }
+    fill(240);
+    text("Frame rate: " + str(Math.round(fr)), 5, 20);
   }
-  fill(240);
-  text("Frame rate: " + str(Math.round(fr)), 5, 20);
 
   // draw indicator
   // fill(200, 0, 0)
   // stroke(0);
   // ellipse(20+10*cos(frameCount/10), 20+10*sin(frameCount/10), 8, 8);
+
+  // draw handle
+  stroke(255);
+  noFill();
+  // ellipse(radius * scaleVar, radius * scaleVar, 2 * 1.02 * radius, 2 * 1.02 * radius)
+  line(radius * (1 + 1.02 * cos(actualAngle)), radius * (1 + 1.02 * sin(actualAngle)), radius * (1 + 1.07 * cos(actualAngle)), radius * (1 + 1.07 * sin(actualAngle)))
+  arc(radius, radius, radius * 1.02 * 2, radius * 1.02 * 2, actualAngle - 0.3, actualAngle + 0.3);
+  // arc(radius * scaleVar, radius * scaleVar, radius * scaleVar * 1.07 * 2, radius * scaleVar * 1.07 * 2, actualAngle - 0.1, actualAngle + 0.1);
+  // arc(radius * scaleVar, radius * scaleVar, radius * scaleVar * 1.02 * 2, radius * scaleVar * 1.02 * 2, actualAngle + 0.17, actualAngle + 0.27);
+  // arc(radius * scaleVar, radius * scaleVar, radius * scaleVar * 1.02 * 2, radius * scaleVar * 1.02 * 2, actualAngle - 0.27, actualAngle - 0.17);
+  // ellipse(radius * scaleVar * (1 + cos(actualAngle)), radius * scaleVar * (1 + sin(actualAngle)), 10, 10);
+  // ellipse(radius * scaleVar * (1 + 1.01 * cos(actualAngle)), radius * scaleVar * (1 + 1.01 * sin(actualAngle)), 10, 10);
+
+  // positionArray = Array.from(Array(numberOfPins).keys()).map((v) => {
+  //   return [radius * (1 + 0.99*cos(2*v/numberOfPins*Math.PI + actualAngle)), radius * (1 + 0.99*sin(2*v/numberOfPins*Math.PI + actualAngle))]
+  // });
 
   // show video sample
   translate(radius * distanceBetweenCenters, 0);
@@ -287,11 +350,18 @@ function draw() {
   for(let i = 0; i < res; i++) {
     for(let j = 0; j < res; j++) {
       if(Math.pow(i-(res-1)/2, 2) + Math.pow(j-(res-1)/2, 2) <= Math.pow((res-1)/2, 2) + 1) {
-        fill(videoSample[i][j]);
+        fill(videoSample[i][j], handleMap(actualAngle, 0, 255, false));
         rect(i*blockLength, j*blockLength, blockLength * 1.05, blockLength * 1.05);
       }
     }
   }
+
+  // reset handle
+  if(!draggingHandle) {
+    handleAngle -= (handleAngle + Math.PI / 4) / (Math.PI / 2) / 8 * (deltaTime * 0.06);
+  }
+
+  // timerCkpt();
 }
 
 function windowResized() {
@@ -302,4 +372,26 @@ function windowResized() {
   positionArray = Array.from(Array(numberOfPins).keys()).map((v) => {
     return [radius * (1 + 0.99*cos(2*v/numberOfPins*Math.PI)), radius * (1 + 0.99*sin(2*v/numberOfPins*Math.PI))]
   });
+}
+
+function keyTyped() {
+  if(key === 'f' || key === 'F') {
+    showFrameRate = !showFrameRate;
+  }
+}
+
+function mousePressed() {
+  // console.log("pressed");
+  draggingHandle = true;
+  mouseStartPosition = [mouseX, mouseY];
+}
+
+function mouseReleased() {
+  draggingHandle = false;
+  handleAngle += handleAngleDragging;
+  handleAngleDragging = 0;
+}
+
+function mouseDragged() {
+  handleAngleDragging = 2 * map(mouseY - mouseStartPosition[1], -height/2, height/2, -Math.PI/4, Math.PI/4);
 }
